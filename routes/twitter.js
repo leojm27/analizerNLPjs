c647e0c77f04064895f4eaafbf71d52d053ed79b
Marianela Leiva <Marianela.Leiva@tsoftglobal.com>
2021-06-11 18:02:37 -0300

    Palabras de entrenamiento para twitter

--- routes/twitter.js
+++ routes/twitter.js
@@ -10,8 +10,15 @@ router.post('/', function (req, res, next) {
   array.forEach(async (element) => {
 
     const resp = await analizeSentiment(element.text)
-    console.log(`\nrespuesta ID ${element.id} : `);
-    console.log(resp);
+
+    if(resp){
+      const { utterance, classifications, intent } = resp;
+      console.log(`\nrespuesta ID ${element.id} : `);
+      console.log("text: " + utterance);
+      console.log("clasificacion: ", classifications);
+      console.log("intent: " + intent);
+    }
+
 
   });
 
@@ -22,31 +29,16 @@ router.post('/', function (req, res, next) {
 
 const analizeSentiment = async (text) => {
 
-  manager.addDocument('es', 'tarjeta problemas', 'Tarjetas problemas técnicos');
-  manager.addDocument('es', 'tarjeta', 'Tarjetas servicios');
-  manager.addDocument('es', 'Financiamiento de hasta 12 cuotas sin interés mediante la utilización de tarjetas de crédito del Banco Macro S.A. y Banco Credicoop con un reintegro del 20', 'Tarjetas servicios');
-  manager.addDocument('es', 'estoy por comprar una máquina con tarjeta de crédito cabal del banco Credicoop mi tarjeta', 'Tarjetas servicios');
-  manager.addDocument('es', 'Por favor acepten la tarjeta CABAL ya que es una TARJETA Nacional y el BANCO CREDICOOP NECESITA apoyarlo', 'Tarjetas servicios');
-  manager.addDocument('es', '20% DE AHORRO CON TARJETAS DE CRÉDITO/DÉBITO DE BANCO CREDICOOP: PROMOCIÓN VÁLIDA PARA LOS MARTES DE SEPTIEMBRE 2020', 'Tarjetas servicios');
-  manager.addDocument('es', 'Acceder a home banking y a mi credicoop movil y cuando pongo la contraseña me dice operador no habilitado para operar', 'Banca Online');
-  manager.addDocument('es', 'Credicoop una transferencia', 'Banca Online');
-  manager.addDocument('es', 'Se cayó el @FBCredicoop? No puedo ni desde la app ni desde la página web A alguien más le pasó? #Credicoop', 'Banca Online');
-  manager.addDocument('es', 'poco práctico la banca x internet  la móvil de terror', 'Banca Online');
-  manager.addDocument('es', 'estoy usando el home banking del banco credicoop', 'Banca Online');
-  manager.addDocument('es', 'Alguien sabe cómo puedo sacar turno en el banco Credicoop', 'Atención Canales');
-  manager.addDocument('es', '@BancoCredicoop Necesito realizar una consulta operativa y el 0800 nadie atiende', 'Atención Canales');
-  manager.addDocument('es', '@BancoCredicoop recién llame atención al cliente Tel 48588573 atendió una mujer que no entendí su nombre , le ped? https://t.co/t4OfkLQY5m', 'Atención Canales');
-  manager.addDocument('es', 'RT. En plena cuarentena dejan a los jubilados sin tarjeta de débito y no dan turnos.', 'Atención Canales');
-  manager.addDocument('es', '', 'OtrasConsultas&Reclamos');
-  manager.addDocument('es', 'Me bloquearon la clave token para la aplicacion movil', 'APP Credicoop');
-  manager.addDocument('es', 'aplicacion de credicoop no soy operador habilitado', 'APP Credicoop');
+  manager.addDocument('es', 'bloqueo, inhabilitado, no puedo acceder, blanquear, clave erronea, no estar registrado', 'Tarjetas problemas técnicos');
+  manager.addDocument('es', 'planes, adicionales, financiamiento, financiación, descuentos, tarjetas, servicios, pedidos, consultas, compras, visa, débito, crédito, mastercard, dar de baja, promociones, cuotas, reintegro, cabal', 'Tarjetas servicios');
+  manager.addDocument('es', 'Acceso, homebanking, movil, banca internet,resumen, digital, chrome, navegador, transferencia, token, fuera de servicio, alias, cbu, clave, usuario, cuenta, acreditar, débito automático, web, caido', 'Banca Online'),
+  manager.addDocument('es', 'app, instalar, aplicación móvil, resumen, digital, ', 'App Credicoop');
+  manager.addDocument('es', 'teléfono, llamar, envío, emails, atender, demora, redes sociales, llamada en espera, mensajes, tiempo, hablar con un representante, derivar llamada, minutos, turnos, paciencia, mala, atención, opciones', 'Atención Canales');
+  manager.addDocument('es', 'dinero, extracción, depósito, cajero, fuera de servicio, no funciona, no tiene plata, sobre, red link, banelco, cabal, billetes, cheques, blanqueo, claves', 'Cajeros automaticos');
+  manager.addDocument('es', 'pymes, cooperativas, industriales, entidades argentinas, informativos, agricultores federales, gerente, emprendedor, presentar, videoconferencia, recaudación, comisión, directivos, comunidad, referentes', 'Influencer');
+  manager.addDocument('es', 'sucursal, consulta, reclamo, estafa, burocracia, falta de pago', 'OtrasConsultas&Reclamos');
   manager.addDocument('es', 'RT.', 'OtrasOpiniones&RT');
-  manager.addDocument('es', 'El problema es que los cajeros están sin dinero', 'Cajeros automaticos');
-  manager.addDocument('es', 'Nación, provincia y Credicoop te larga billetes de mil y de quinientos. Los privados todos billetes de cien.', 'Cajeros automaticos');
-  manager.addDocument('es', 'En ningún banco provincia de Quilmes, Berazategui o Varela o los Credicoop q adhieren a red link están?', 'Cajeros automaticos');
-  manager.addDocument('es', 'Me bloquearon la clave token para la aplicacion movil', 'APP Credicoop');
-  manager.addDocument('es', 'aplicacion de credicoop no soy operador habilitado', 'APP Credicoop');
-  manager.addDocument('es', 'RT.', 'OtrasOpiniones&RT');
+  manager.addDocument('es', 'campaña, proyectos, asistencia técnica, fundación, universidades, entrevistas, conferencias, días festivos, saludos, instituciones, filial, afiliados, sindicatos, municipios, capacitaciones, denuncias, zoom, meeting, becas, tecnologías, convenio, acuerdos, donaciones', 'prensa');
 
   await manager.train();
   manager.save();
